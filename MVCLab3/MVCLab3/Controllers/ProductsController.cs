e1ae7bf562d41d14f3a0ece83aa382a6cbf9f5f3
Eleasar <S01209223@acad.tri-c.edu>
2020-02-02 22:21:45 -0500

    Commit Lab3

---
+++ MVCLab3/MVCLab3/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLab3.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public String Index()
+        {
+            return "Product/Index is displayed";
+        }
+        public String Browse()
+        {
+            return "Browse displayed";
+        }
+        public string Details(int id)
+        {
+            string message = "Details displayed for ID = " + id;
+            return message;
+        }
+
+        public String Location(string zip)
+        {
+            string message = HttpUtility.HtmlEncode("Location displayed for zip = " + zip);
+            return message;
+        }
+    }
+}
---
+++ MVCLab3/MVCLab3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLab3.MvcApplication" Language="C#" %>
